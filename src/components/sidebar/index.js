da7a799f8f7b2dbe4d50754972a19babb315a9c7
oleksiypolonka <polonka.oleksiy@gmail.com>
2018-04-08 12:33:47 +0300

    basic sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+import * as R from 'ramda';
+import uuid from 'uuid';
+
 class Sidebar extends Component {
+  renderSystemFolders (folder) {
+    return <li key={uuid()}>{folder}</li>
+  }
+
+  getUniqFolders (folders) {
+    return R.compose(
+      R.uniq,
+      R.map(el => el.split('/')[1]),
+      R.values
+    )(folders)
+  }
+
   render () {
+    const { systemFolders } = this.props;
     return (
       <nav>
+        <ul>
+          { R.map(this.renderSystemFolders, this.getUniqFolders(systemFolders)) }
+        </ul>
       </nav>
     );
   }
